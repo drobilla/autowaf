7d2d0fb683240a99faf2c5bd2db00d679e8052cb
David Robillard <d@drobilla.net>
2011-03-27 19:46:10 +0000

    Add custom waf building script.
    git-svn-id: http://svn.drobilla.net/autowaf@38 e2e4594f-ea7b-45dc-bc5a-5f5301e603aa

---
+++ build_custom_waf.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Run this script from the waf source directory to build a custom waf script
+# which includes autowaf (and the other listed tools)
+
+./waf-light -v --make-waf --tools=doxygen,swig,gnu_dirs,/path/to/autowaf/autowaf.py --prelude=''
