ef01c43f904ecf8977ab4540239e7b3ea2666285
David Robillard <d@drobilla.net>
2011-07-07 19:19:31 +0000

    Eliminate need to modify build_custom_waf.sh in order to use it
    git-svn-id: http://svn.drobilla.net/autowaf@48 e2e4594f-ea7b-45dc-bc5a-5f5301e603aa

--- build_custom_waf.sh
+++ build_custom_waf.sh
@@ -2,4 +2,11 @@
 # Run this script from the waf source directory to build a custom waf script
 # which includes autowaf (and the other listed tools)
 
-./waf-light -v --make-waf --tools=doxygen,swig,/path/to/autowaf/autowaf.py --prelude=''
+if [ $# != 1 ]; then
+    echo "Error: Path to autowaf.py must be passed as only argument"
+    exit 1
+fi
+
+autowaf_py=$1
+
+./waf-light -v --make-waf --tools=doxygen,swig,$autowaf_py --prelude=''
